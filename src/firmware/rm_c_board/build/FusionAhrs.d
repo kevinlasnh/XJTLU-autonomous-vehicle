ae34690bf327b4098efb8bccd1d662119766ee28
Yangbadger222 <badger@YangbadgerdeMacBook-Air.local>
2026-04-20 21:39:53 +0800

    chore: remove build artifacts from tracking

--- src/firmware/rm_c_board/build/FusionAhrs.d
+++
@@ -1,5 +0,0 @@
-build/FusionAhrs.o: Core/Src/FusionAhrs.c Core/Inc/FusionAhrs.h \
- Core/Inc/FusionConvention.h Core/Inc/FusionMath.h
-Core/Inc/FusionAhrs.h:
-Core/Inc/FusionConvention.h:
-Core/Inc/FusionMath.h:
--- src/firmware/rm_c_board/build/FusionOffset.d
+++
@@ -1,4 +0,0 @@
-build/FusionOffset.o: Core/Src/FusionOffset.c Core/Inc/FusionOffset.h \
- Core/Inc/FusionMath.h
-Core/Inc/FusionOffset.h:
-Core/Inc/FusionMath.h:
